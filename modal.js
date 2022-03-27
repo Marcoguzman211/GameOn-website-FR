0705b8a86b3900eddf41f18fc348bee33d7da6c6
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2022-03-28 00:17:09 +0200

    premiere version js

--- modal.js
+++ modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -7,11 +7,31 @@ function editNav() {
   }
 }
 
+//REGEX
+const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const BIRTHDATE_REGEX = /^\d{4}\-\d{2}\-\d{2}$/;
+const QUANTITY_REGEX = /^[0-9]+$/;
+const NAMES_REGEX = /^[a-zA-Z\-]+$/;
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBtnSubmit = document.querySelector(".btn-submit");
 const formData = document.querySelector('#form');
+
+//Formulaire
+//Elements
+const inputFirstName = document.getElementById('first');
+const inputLastName = document.getElementById('last');
+
+//Messages d'Erreur
+const errorFirstName = document.getElementById("error_first"); // ajouté
+const errorLastName = document.getElementById("error_last"); // ajouté
+console.log(errorLastName)
+/*const errorEmail = document.getElementById("error_email"); // ajouté
+const errorBirthdate = document.getElementById("error_birthdate"); // ajouté
+const errorQuantity = document.getElementById("error_quantity"); // ajouté*/
+
 
 // launch modal form and event
 const launchModal = () => modalbg.style.display = "block";
@@ -26,21 +46,60 @@ closeBtn.addEventListener("click", hideModal)
 
 
 //Validation firstName input value
-const inputText = document.getElementById('first');
-inputText.addEventListener('keyup', () => {
-  console.log("Tu tapes dans prénom...")
+inputFirstName.addEventListener('keyup', () => {
+  if (inputFirstName.value.length < 2) {
+    inputFirstName.classList.add("input-error");
+  } else {
+    inputFirstName.classList.remove('input-error')
+  }
 })
 
 const validateFirstNameInput = () => {
-  console.log(inputText.value.length)
+  if (inputFirstName.value.length >= 2 && inputFirstName.value.match(NAMES_REGEX)) {
+    return true
+  } else {
+    errorFirstName.classList.add("form-error-message")
+    errorFirstName.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    return false
+  }
+}
 
+//Validation Last Name
+inputLastName.addEventListener('keyup', (e) => {
+  if (inputLastName.value.length < 2) {
+    inputLastName.classList.add("input-error");
+  } else {
+    inputLastName.classList.remove('input-error')
+  }
+})
+
+const validateLastNameInput = () => {
+  if (inputLastName.value.length >= 2 && inputLastName.value.match(NAMES_REGEX)) {
+    console.log("Nom de famille a marché")
+    return true
+  } else {
+    errorLastName.classList.add("form-error-message")
+    errorLastName.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    return false
+  }
 }
 
 
 //Function finale pour valider le formulaire
+let formValidated
 const validate = (e) => {
   e.preventDefault()
-  validateFirstNameInput()
+  if (!validateLastNameInput() === true) {
+    formValidated = false;
+  } else if (!validateFirstNameInput() === true) {
+    formValidated = false;
+  } else {
+    formValidated = true
+  }
+
+  if (formValidated === true) {
+    console.log('Yeeeesss')
+  }
 
 }
 formData.addEventListener('submit', validate)
